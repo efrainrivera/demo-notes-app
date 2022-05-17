6f496c321d5293a0586ef80bc4f642551a34d816
efrainrivera <efrariverac@gmail.com>
2022-05-17 12:41:29 +1000

    Setting up the React app

---
+++ frontend/src/Routes.js
@@ -0,0 +1,19 @@
+import NotFound from "./containers/NotFound.js";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+
+export default function Routes() {
+  return (
+    <Switch>
+        <Route exact path="/">
+        <Home />
+        </Route>
+        
+        {/* Finally, catch all unmatched routes */}
+        <Route>
+            <NotFound />
+        </Route>
+    </Switch>
+  );
+}
